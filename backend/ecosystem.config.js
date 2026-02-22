4f3fd6188a55ce79845a4dae4858a489dab1b2c1
a-small-coder <hulktv222@mail.ru>
2026-02-22 19:51:14 +0300

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:a-small-coder/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp "${path.resolve(__dirname, '.env').replace(/\\\\/g, '/')}" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp "${path.resolve(__dirname, '.env').replace(/\\/g, '/')}" "${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}"`,
       'post-deploy': 'npm i && npm run build',
     },
   },
