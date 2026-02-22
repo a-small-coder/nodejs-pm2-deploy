338bc57e1244393d5fa4173c24e57f70f0628307
a-small-coder <hulktv222@mail.ru>
2026-02-22 19:34:18 +0300

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,5 +1,7 @@
 require('dotenv').config({ path: '.env.deploy' });
 
+const path = require('path');
+
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
 } = process.env;
@@ -17,7 +19,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:a-small-coder/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp "${path.resolve(__dirname, '.env')}" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
